533af2eb31096ac212dcef7aac2a5f20e16bb5b5
Rakshith J M <rakshith.jm.555@gmail.com>
2024-09-27 06:42:53 +0000

    EKS_Terraform/ files added

--- EKS_Terraform/variables.tf
+++ EKS_Terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
+  default = "K8"
 }
 
 variable "instance_type" {
   description = "Instance Type For EC2 Instance"
   type        = string
+  default = "t2.large"
 }
